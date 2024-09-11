67922bd9c48d3e47c3efad92d5023b7d31b2d1e6
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2024-09-11 01:41:42 -0600

    Interfaz de ProductView
    Se mejoró la interfaz en el archivo ProductView para mostrar de manera más estética los detalles del producto seleccionado. También se modificó el archivo info.plist para bloquear las orientaciones landscape en la App.

--- Store4U/Store4U/View/ProductView.swift
+++ Store4U/Store4U/View/ProductView.swift
@@ -20,9 +20,66 @@ struct ProductView: View {
                 } else {
                     // Detalle de producto seleccionado.
                     if let producto = selectedProduct {
-                        Text("Producto seleccionado: \(producto.nombre)")
-                        Text("Precio: \(producto.precioRegular)")
+                        // Nombre del producto
+                        Text(producto.nombre)
+                            .font(.title)
+                            .padding(.vertical)
+                        // Imagen asíncrona cargada del link obtenido de la API
+                        AsyncImage(url: URL(string: producto.urlImagenes[0])) { phase in
+                            if let image = phase.image {
+                                image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                            } else if phase.error != nil {
+                                Text("No se pudo cargar la imagen")
+                            } else {
+                                ProgressView()
+                            }
+                        }
+                        .frame(width: 200, height: 200)
+                        // Letrero de descuento
+                        if producto.descuento {
+                            HStack {
+                                Spacer()
+                                Image(systemName: "banknote")
+                                    .foregroundStyle(.green)
+                                Text("¡En descuento!")
+                                    .font(.footnote)
+                                    .fontWeight(.bold)
+                            }
+                            .padding(.vertical, -20)
+                            .padding(.horizontal)
+                        }
+                        // Precio regular vs precio final
+                        HStack {
+                            Text("$\(producto.precioRegular)")
+                                .font(.title)
+                            if producto.descuento {
+                                Image(systemName: "arrow.forward")
+                                    .font(.title)
+                                    .bold()
+                                Text("$" + String(format: "%.0f", producto.precioFinal))
+                                    .font(.title)
+                            }
+                            Spacer()
+                        }
+                        .padding(.horizontal)
+                        // Categoria
+                        HStack {
+                            Text("Categoría: ")
+                                .font(.title3)
+                            Image(systemName: "smartphone")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30, height: 30)
+                                .foregroundStyle(.red)
+                            Text(producto.codigoCategoria)
+                                .font(.title3)
+                            Spacer()
+                        }
+                        .padding(.horizontal)
                     }
+                    Spacer()
                 }
                 NavigationLink(destination: ListView(response: $response, selectedProduct: $selectedProduct)) {
                     Text("Mostrar listado")
